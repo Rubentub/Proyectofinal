b475915bea2cc0511668cd03841e3c6754a425d8
Izan5 <114390002+Izan5@users.noreply.github.com>
2026-06-09 12:00:48 +0200

    Update conexion.php to load config for DB connection
    Refactor database connection to use config file.

--- conexion.php
+++ conexion.php
@@ -1,12 +1,17 @@
 <?php
-$host = "192.168.15.162";
-$usuario = "gsuser";
-$contraseña = "1234Gs@";
-$base_datos = "gsbase";
+$config = include("config.php");
 
-$conn = new mysqli($host, $usuario, $contraseña, $base_datos);
+$conn = new mysqli(
+    $config["host"],
+    $config["user"],
+    $config["password"],
+    $config["db"]
+);
 
 if ($conn->connect_error) {
-    die(json_encode(["status" => "error", "msg" => "Error de conexión: " . $conn->connect_error]));
+    die(json_encode([
+        "status" => "error",
+        "msg" => "Error de conexión: " . $conn->connect_error
+    ]));
 }
 ?>
